96e898a3d4ea55c1d4bf5bdcae2b3f2969bd9f36
pmarcelacademy1 <pmarcelacademy@gmail.com>
2026-08-14 10:43:48 +0100

    all files believed to be linked to config

--- public/js/config.js
+++ public/js/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'swiftscapitals-backend.onrender.com';
+const API_BASE_URL = 'https://swiftscapitals-backend.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
